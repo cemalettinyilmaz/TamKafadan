e5940059ed25c0991dbc15f94ff637215ac291a7
Cemalettin Yılmaz <cemalettin0yilmaz@gmail.com>
2022-04-18 13:33:04 +0300

    .

--- TamKafadan/Controllers/HomeController.cs
+++ TamKafadan/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -6,6 +8,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using TamKafadan.Models;
+using TamKafadan.ViewModels;
 
 namespace TamKafadan.Controllers
 {
@@ -22,7 +25,37 @@ namespace TamKafadan.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            var kadi = HttpContext.Session.GetString("kullaniciAdi");
+            Yazar girisYapan = _db.Yazarlar.Include(x => x.Konulari).Include(x=>x.Makaleleri).FirstOrDefault(x => x.KullaniciAdi == kadi);
+
+            HomeMakaleViewModel vm = new HomeMakaleViewModel(); 
+
+            vm.EnCokOkunanBesMakale=_db.Makaleler.Include(x=>x.Yazar).Take(6).Where(x=>x.OlusuturulmaZamani.Month==DateTime.Now.Month && x.OnayliMi==true).OrderByDescending(x=>x.GoruntulenmeSayisi).ToList();
+
+            if (girisYapan != null)
+            {
+                 List<Konu> konular = new List<Konu>();            
+                foreach (var item in girisYapan.Konulari)
+                {
+                    konular.Add(_db.Konular.Include(x=>x.Yazarlar).Include(x => x.Makaleler).FirstOrDefault(x => x.KonuAdi == item.KonuAdi));
+                }
+                List<Makale> gonderilecekMakaleler=new List<Makale>();
+                foreach (var item in konular)
+                {
+                    foreach (var makale in item.Makaleler)
+                    {
+                        gonderilecekMakaleler.Add(makale);
+                    }
+                }
+                vm.KullaniciKonuMakaleleri = gonderilecekMakaleler.Distinct().ToList();
+            }
+            else
+            {
+                vm.ZiyaretciRastgeleMakaleleri=_db.Makaleler.Take(10).Where(x=>x.OnayliMi==true).ToList();
+            }
+
+
+            return View(vm);
         }
 
         public IActionResult Privacy()
---
+++ TamKafadan/ViewModels/HomeMakaleViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using TamKafadan.Models;
+
+namespace TamKafadan.ViewModels
+{
+    public class HomeMakaleViewModel
+    {
+        public List<Makale> EnCokOkunanBesMakale { get; set; }
+        public List<Makale> KullaniciKonuMakaleleri { get; set; }
+        public List<Makale> ZiyaretciRastgeleMakaleleri { get; set; }
+    }
+}
